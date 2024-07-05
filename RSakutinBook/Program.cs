fe7e279b0f768b7fa3c96d7534cfb63b574aeffb
DELLOON <delloondev@gmail.com>
2024-07-05 14:19:30 +0800

    Добавьте файлы проекта.

---
+++ RSakutinBook/Program.cs
@@ -0,0 +1,30 @@
+﻿using RSakutinBook.TaskPractice.CSharpBasics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSakutinBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayPractice arrayPractice = new ArrayPractice();
+
+            Console.WriteLine("Практика массивов: ");
+
+            Console.WriteLine("Задача 2:");
+            arrayPractice.SecondTask();
+
+            Console.WriteLine("\nЗадача 3:");
+            arrayPractice.ThirdTask();
+
+            Console.WriteLine("\nЗадача 4:");
+            arrayPractice.FourthTask();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RSakutinBook/TaskPractice/CSharpBasics/ArrayPractice.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSakutinBook.TaskPractice.CSharpBasics
+{
+    internal class ArrayPractice
+    {
+        //Задача 1
+        //Объявите переменную, которая хранит массив, тип
+        //элементов у которого string и поместите туда ссылку
+        //на новый массив с типом элементов string,
+        //размерностью в 5 элементов.
+        //Далее, через оператор индексации,
+        //проинициализируйте каждый элемент.
+        public void FirstTask()
+        {
+            string[] stringArray = new string[5];
+            stringArray[0] = "Иванов";
+            stringArray[1] = "Иван";
+            stringArray[2] = "Иванович";
+            stringArray[3] = "C# - программист";
+            stringArray[4] = "Разработчик игр";
+        }
+
+        //Задача 2
+        //Напишите программу, которая выведет все элементы массива.
+        public void SecondTask()
+        {
+            int[] numbers = new int[3];
+            numbers[0] = 1;
+            numbers[1] = 15;
+            numbers[2] = 2;
+
+            Console.WriteLine("Массив: ");
+
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"Элемент [{i+1}] = {numbers[i]}");
+            }
+        }
+
+        //Задача 3
+        //Напишите программу, которая выведет сумму всех элементов массива.
+        public void ThirdTask()
+        {
+            int[] numbers = new int[3];
+            numbers[0] = 1;
+            numbers[1] = 15;
+            numbers[2] = 2;
+
+            int numberSumm = 0;
+
+            Console.WriteLine("Массив: ");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"Элемент [{i + 1}] = {numbers[i]}");
+                numberSumm = numberSumm + numbers[i];
+            }
+
+            Console.WriteLine($"Сумма всех элементов массива: {numberSumm}");
+        }
+
+        //Задача 4
+        //Напишите программу, которая выведет максимальное число в массиве.
+        public void FourthTask()
+        {
+            int[] numbers = new int[3];
+            numbers[0] = 1;
+            numbers[1] = 15;
+            numbers[2] = 2;
+
+            int maxNumber = numbers[0];
+
+            Console.WriteLine("Массив: ");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"Элемент [{i + 1}] = {numbers[i]}");
+                
+                if(numbers[i] > maxNumber )
+                {
+                    maxNumber = numbers[i];
+                }
+
+            }
+
+            Console.WriteLine($"Макисмальное число в массиве: {maxNumber}");
+        }
+
+    }
+}
